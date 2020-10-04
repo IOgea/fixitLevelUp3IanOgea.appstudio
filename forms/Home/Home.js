ecd5c900a6e0ce4c5f9cd8b30e805efbecc4f51f
IOgea <Irogea@gmail.com>
2020-10-04 00:11:51 -0500

    got close to finishing car loan

---
+++ forms/Home/Home.js
@@ -0,0 +1,26 @@
+
+//changes placeholder to indicate the time input should be in months
+btnCarLoan.onclick=function(){
+  inptTime.placeholder = "Enter the number of months"
+}
+
+//changes placeholder to indicate the time should either be 15 or 30 years
+btnHomeLoan.onclick = function(){
+  inptTime.placeholder  = "Enter 15 or 30 years"
+}
+
+//function to calculate car payment
+function carLoanPayment(principle,months,rate){
+  rate = rate/1200
+  payment = principle * (rate*((1+rate)**months))/(((1+rate)**months)-1)
+  payment = Math.round(payment)
+  return payment
+  }
+  
+  //button to calculate payment and give the output
+btnCalc.onclick = function(){
+if (inptTime.placeholder == "Enter the number of months"){
+  lblOutput.value = `A car loan for $${inptAmount.value} over ${inptTime.value} months at ${inptIntRate.value}% interest would have a monthly payment of 
+  ${carLoanPayment(parseInt(inptAmount.value),parseInt(inptTime.value),parseInt(inptIntRate.value))}`
+  }
+  }
